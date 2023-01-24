3c3a81a60e2d93d8480b5ee08522e55707bd6a31
Naomi-Morales13 <122810179+Naomi-Morales13@users.noreply.github.com>
2023-01-24 20:11:29 +0000

    Se creo el programa2.py

---
+++ semana2/programa3.py
@@ -0,0 +1,10 @@
+"""
+    programa3.py
+    #nombre: Naomi Morales
+    #Fecha: 24/01/2023
+    #Deacripcion:
+"""
+numero1 = 10
+numero2 = 5
+print(numero1 + numero2) # 15
+print("{} + {}".format(numero1 , numero2))
