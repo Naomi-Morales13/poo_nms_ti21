645811376733f9cfa6a38025b22eed5036380716
Naomi-Morales13 <122810179+Naomi-Morales13@users.noreply.github.com>
2023-01-24 19:40:27 +0000

    se creó el programa1.py

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 |No1.|Archivo|Descrpción|
 |--|--|--|
 |1.|programa0.py|Hola mundo en python|
+|2.|programa1.py|Concatenación de variables
---
+++ semana2/programa1.py
@@ -0,0 +1,14 @@
+"""
+    Programa1
+    Nombre: Naomi Morales Sanchez
+    Fecha: 23/01/2023
+    Descrpción: comentarios multilinea, comentarios de una linea y concatenacion de cadenas, cadena de caracteres
+"""
+variable1 = "Hola "  # Variable que almacena una cadena de caracteres 
+variable2 = "Python"  # Variable que almacena una cadena de caracteres 
+print(variable1 + variable2)  # Concatena cadenas
+"""
+variable1 = "Hola "  # Variable tipo str
+variable2 = '619'  # Variable tipo int
+print(variable1 + variable2)  # Imprime la concatenación de 2 variables tipo str
+"""
